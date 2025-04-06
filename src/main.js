4074d9f92449b848ea164bdeb9996cc7b890d789
Некрасов Владислав <nebor2309@gmail.com>
2025-04-06 17:00:10 +0500

    21

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import MainPresenter from './presenter';
+import MainPresenter from './presenter/mainPresenter';
 
 const siteBodySortElement = document.querySelector('.trip-events');
 const mainPresenter = new MainPresenter({container: siteBodySortElement});
